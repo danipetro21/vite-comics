262a6ebd883b6efdd3b3e64b87b0612c77272ea2
danipetro21 <idany346@gmail.com>
2022-12-12 21:07:06 +0100

    add header and footer

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,141 @@
+<script>
+
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                }
+
+            ]
+        }
+    },
+}
+
+</script>
+
+<template>
+
+    <header>
+
+        <div class="container">
+            <a href="">
+                <img src="public/img/dc-logo.png" alt="">
+
+            </a>
+
+            <nav>
+                <ul>
+                    <div :class="{link: link.current}" v-for="(link, index) in links" :key="index">
+                        <li>
+                            <a :href="link.url" :class="{ active: link.current }">
+                                {{ link.text }}
+                            </a>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
+
+        </div>
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+
+header {
+    text-align: center;
+
+
+}
+
+.container {
+    @include center();
+    max-width: 1400px;
+    height: 150px;
+
+    margin: 0 auto;
+
+    img {
+        width: 100%;
+    }
+
+    .link {
+        height: 150px;
+        border-bottom: 6px $text_color solid;
+        display: flex;
+        align-items: center;
+    }
+
+}
+
+ul {
+    list-style: none;
+    @include center();
+
+    li a {
+        padding: 15px;
+        color: black;
+        text-decoration: none;
+        transition: background 0.3s;
+        font-size: 0.9rem;
+        
+        &.active,
+        &:hover {
+            color: $text_color;
+        }
+    }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,110 @@
+<script>
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+
+        <div class="container content">
+            <div class="box">
+                <h1> --> Content goes here!!! </h1>
+            </div>
+        </div>
+
+
+        <div class="container service">
+
+            <div class="box">
+
+                <ul>
+                    <li>
+                        <img src="public/img/buy-comics-digital-comics.png" alt="">
+                        <span>DIGITAL COMICS</span>
+                    </li>
+
+                    <li>
+                        <img src="public/img/buy-comics-merchandise.png" alt="">
+                        <span>DC MERCHANDISE</span>
+                    </li>
+
+                    <li>
+                        <img src="public/img/buy-comics-subscriptions.png" alt="">
+                        <span>SUBSCRIPTION</span>
+                    </li>
+
+                    <li>
+                        <img src="public/img/buy-comics-shop-locator.png" alt="">
+                        <span>COMIC SHOP LOCATOR</span>
+                    </li>
+
+                    <li>
+                        <img src="public/img/buy-dc-power-visa.svg" alt="">
+                        <span>DC POWER VISA</span>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+
+main {
+    width: 100%;
+    height: 300px;
+    border: 1px solid black;
+}
+
+.container {
+    width: 100%;
+    height: 150px;
+}
+.box{
+    max-width: 1400px;
+    margin: 0 auto;
+    height: 100%;
+}
+
+.content {
+    background-color: $bg_secondary;
+    color: $primary_color;
+}
+
+.service {
+    background-color: $bg_primary ;
+
+    ul {
+        display: flex;
+        height: 100%;
+        justify-content: space-around;
+        align-items: center;
+        color: $primary_color;
+
+    }
+
+    li{
+        display: flex;
+        align-items: center;
+        gap: 7px;
+        font-size: 0.9rem;
+    }
+
+    img {
+        width: 60px;
+        height: 60px;
+        object-fit:contain;
+    }
+}
+</style>
